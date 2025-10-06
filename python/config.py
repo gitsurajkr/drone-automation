0262793cb89049bd313908f92206d9a33465c423
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2025-10-06 13:05:00 +0530

    corect the connection string for testing

--- python/config.py
+++ python/config.py
@@ -6,7 +6,7 @@ TELEMETRY_INTERVAL = 1
 # Connection settings
 # For hardware drone, use: "/dev/ttyACM0" or "/dev/ttyUSB0"
 # For SITL, use: "tcp:127.0.0.1:5760" or "udp:127.0.0.1:14550"
-DEFAULT_CONNECTION_STRING = "udp:127.0.0.1:14550"
+DEFAULT_CONNECTION_STRING = "tcp:127.0.0.1:5760"
 DEFAULT_BAUD_RATE = 115200
 
 # DEPRECATED: Use SITLConfig class instead
